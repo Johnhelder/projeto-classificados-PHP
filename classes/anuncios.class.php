93ee0de1fb223de43fcf0547cad6c6ef5eb7360c
John Helder <johnhelderjp@gmail.com>
2022-04-26 19:13:01 -0300

    Foi criado add-anuncio e anuncios.class

--- classes/anuncios.class.php
+++ classes/anuncios.class.php
@@ -4,7 +4,7 @@ class Anuncios{
         global $pdo;
 
         $array = array();
-        $sql = $pdo->prepare("SELECT *, (SELECT anuncios_imagens WHERE anuncios_imagens.id_anuncio = anuncio.id LIMIT 1) as url FROM anuncios WHERE id_usuario = :id_usuario");
+        $sql = $pdo->prepare("SELECT *, (SELECT anuncios_imagens WHERE anuncios_imagens.id_anuncio = anuncios.id LIMIT 1) as url FROM anuncios WHERE id_usuario = :id_usuario");
 
         $sql->bindValue(":id_usuario", $_SESSION['cLogin']);
         $sql->execute();
@@ -13,6 +13,20 @@ class Anuncios{
             $array = $sql->fetchAll();
         }
         return $array;
+    }
+
+    public function addAnuncio($titulo, $categoria, $valor, $descricao, $estado){
+        global $pdo;
+
+        $sql =  $pdo->prepare("INSERT INTO anuncios SET titulo = :titulo, id_categoria = :id_categoria, id_usuario = :id_usuario, descricao = :descricao, valor = :valor, estado = :estado");
+        $sql->bindValue(":titulo", $titulo);
+        $sql->bindValue(":id_categoria", $categoria);
+        $sql->bindValue(":id_usuario", $_SESSION['cLogin']);
+        $sql->bindValue(":descricao", $descricao);
+        $sql->bindValue(":valor", $valor);
+        $sql->bindValue(":estado", $estado);
+        $sql->execute();
+
     }
 }
 
