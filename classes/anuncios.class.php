6ea606341325b69428d0ba35a0182713f7c97b09
John Helder <johnhelderjp@gmail.com>
2022-04-27 19:48:10 -0300

    Foram criados mais alguns arquivos

--- classes/anuncios.class.php
+++ classes/anuncios.class.php
@@ -4,7 +4,7 @@ class Anuncios{
         global $pdo;
 
         $array = array();
-        $sql = $pdo->prepare("SELECT *, (SELECT anuncios_imagens WHERE anuncios_imagens.id_anuncio = anuncios.id LIMIT 1) as url FROM anuncios WHERE id_usuario = :id_usuario");
+        $sql = $pdo->prepare("SELECT *, (SELECT anuncios_imagens.url from anuncios_imagens WHERE anuncios_imagens.id_anuncio = anuncios.id LIMIT 1) as url FROM anuncios WHERE id_usuario = :id_usuario");
 
         $sql->bindValue(":id_usuario", $_SESSION['cLogin']);
         $sql->execute();
@@ -13,6 +13,21 @@ class Anuncios{
             $array = $sql->fetchAll();
         }
         return $array;
+    }
+
+    public function getAnuncio(){
+        $array = array();
+        global $pdo;
+
+        $sql = $pdo->prepare("SELECT * FROM anuncios WHERE id = :id");
+        $sql->bindValue(":id", $id);
+        $sql->execute();
+
+        if($sql->rowCount() > 0){
+            $array = $sql->fetch();
+        }
+        return $array;
+
     }
 
     public function addAnuncio($titulo, $categoria, $valor, $descricao, $estado){
@@ -27,6 +42,18 @@ class Anuncios{
         $sql->bindValue(":estado", $estado);
         $sql->execute();
 
+    }
+
+    public function excluirAnuncio($id){
+        global $pdo;
+
+        $sql = $pdo->prepare("DELETE FROM anuncios_imagens WHERE id_anuncio = :id_anuncio");
+        $sql->bindValue(":id_anuncio", $id);
+        $sql->execute();
+
+        $sql = $pdo->prepare("DELETE FROM anuncios WHERE id = :id");
+        $sql->bindValue(":id", $id);
+        $sql->execute();
     }
 }
 
